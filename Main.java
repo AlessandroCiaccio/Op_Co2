fbe5094a72dbbbd765e523b623f6389d265bb23d
Alessandro <sandroscro@libero.it>
2023-10-17 12:03:10 +0200

    Aggiunto metodo

--- Main.java
+++ Main.java
@@ -1,8 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        char x='a', y='z';
-        boolean z=(x==y);
+        char x='a', y='A';
         System.out.println("The characters are "+x+" and "+y);
-        System.out.println("They are equal? "+z);
+        System.out.println("They are equal? "+comparazione(x,y));
+    }
+    public static boolean comparazione(char x, char y){
+        return x==y;
     }
 }
